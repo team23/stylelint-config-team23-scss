661da0a56876d52df0b55e6434d9c831a0fb7c38
Manuel Munz <munz@comuno.net>
2019-08-08 10:08:04 +0200

    initial commit, based on stylelint-config-standard

---
+++ README.md
@@ -0,0 +1,9 @@
+# stylelint-config-team23-scss
+
+> Standard SCSS config for TEAM23
+
+Extends [`stylelint-config-recommended`](https://github.com/stylelint/stylelint-config-recommended).
+
+Used plugins:
+ - [`stylelint-scss`](https://github.com/kristerkari/stylelint-scss).
+
---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict"
+
+module.exports = {
+  "extends": "stylelint-config-recommended",
+  "plugins": [
+    "stylelint-scss"
+  ],
+  "rules": {
+    "at-rule-no-unknown": null,
+    "scss/at-rule-no-unknown": true,
+    "at-rule-blacklist": ["debug"],
+    "scss/at-each-key-value-single-line": true,
+    "scss/at-extend-no-missing-placeholder": true,
+    "scss/at-function-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/at-import-no-partial-leading-underscore": true,
+    "scss/at-import-partial-extension-blacklist": ["scss"],
+    "scss/at-mixin-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/dollar-variable-colon-space-after": "always",
+    "scss/dollar-variable-colon-space-before": "never",
+    "scss/dollar-variable-pattern": "^[_]?[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/percent-placeholder-pattern": "^[a-z]+([a-z0-9-]+[a-z0-9]+)?$",
+    "scss/selector-no-redundant-nesting-selector": true
+  },
+}
